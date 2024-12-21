d39395ed213bd64309bb4f8859fbac7c2d8d0a97
sherif ashraf <sherifaway@gmail.com>
2024-12-21 22:34:02 +0200

    signup page include validation logic with custome interactive ui&ux with api requests and log-out logic

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,59 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUserForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filterdUsers = await User.findById({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filterdUsers);
+  } catch (errr) {
+    console.error(errr.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Messages.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    res.status(200).json(newMessage);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,32 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
+// Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+// Start Server
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
