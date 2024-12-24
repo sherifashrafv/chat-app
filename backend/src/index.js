4cdfb50188d22a9aacac96bdc9d3d31d557b79e5
sherif ashraf <sherifaway@gmail.com>
2024-12-24 22:56:33 +0200

    wib

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
-
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
@@ -25,6 +25,14 @@ app.options("*", cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // Start Server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
