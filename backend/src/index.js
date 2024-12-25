8211c33eaf64c1226d21fefe056c1a993210e184
sherif ashraf <sherifaway@gmail.com>
2024-12-25 22:17:13 +0200

    wib

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
-import path from "path";
 
 dotenv.config();
-const PORT = process.env.PORT || 5001;
 
-// Middleware
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -20,13 +21,10 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// production mode
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -34,8 +32,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-// Start Server
+
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
