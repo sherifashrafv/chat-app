f9a3d842e2be97f86678e5f4f39cb112d31b6ab0
sherif ashraf <sherifaway@gmail.com>
2024-12-27 12:50:18 +0200

    .

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://chat-app-k799.onrender.com"],
+    credentials: true,
   },
+  transports: ["websocket", "polling"], // Ensure fallback support
 });
 
 export function getReceiverSocketId(userId) {
