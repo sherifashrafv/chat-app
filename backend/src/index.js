8376f306ff4274aec5408f7347305251cca957e5
sherif ashraf <sherifaway@gmail.com>
2024-12-27 12:27:55 +0200

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chat-app-k799.onrender.com"],
     credentials: true,
   })
 );
